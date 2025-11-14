92cf2b104a2726514230a8b25cb63ffe4cd838b6
bhavikasharma2907 <bhavikasharma2907@gmail.com>
2025-11-14 10:29:18 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,15 @@
+a=int(input("enter 1st number"))
+b=int(input("enter 2st number"))
+choice=input("enter your choice ")
+
+if choice=="+":
+    print(a+b)
+elif choice=="-":
+    print(a-b)
+elif choice=="*":
+    print(a*b)
+
+elif choice=="/":
+        print(a/b)
+else:
+        print("invalid choice")
